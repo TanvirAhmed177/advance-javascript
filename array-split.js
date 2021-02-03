d100b71960f3905fa3992fa6eb9db4e6f0e89bfd
TanvirAhmed177 <ahmed.tanvir.siddik@gmail.com>
2021-02-03 20:20:14 +0600

    Update

---
+++ array-split.js
@@ -0,0 +1,6 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const part = numbers.slice(2, 5);
+// const removed = numbers.splice(2, 5, 77, 88, 99);
+const joint = numbers.join("ami");
+console.log(joint);
+console.log(numbers);
---
+++ scope.js
@@ -0,0 +1,25 @@
+let bonus = 20;
+function sum(first, second) {
+  let result = first + second + bonus;
+  if (result > 9) {
+    var mood = "happy";
+    mood = "Sad";
+    mood = "fishy";
+    console.log(mood);
+  }
+  console.log(mood);
+
+  //   console.log(result);
+  //   console.log(bonus);
+  return result;
+}
+
+console.log(day);
+var day = "friday";
+console.log(day);
+let day = "friday";
+
+const output = sum(6, 7);
+console.log(output);
+// console.log(result);
+console.log(bonus);
